9819295ea84bbcd9ff8ab22fcc825dfda301df8b
Raysill <taichi3344@gmail.com>
2025-08-10 06:25:29 +0900

    Delete kari.js

--- kari.js
+++
@@ -1,8 +0,0 @@
-// ページ読み込み時のアニメーション例
-document.addEventListener('DOMContentLoaded', () => {
-  document.body.style.opacity = 0;
-  setTimeout(() => {
-    document.body.style.transition = 'opacity 1s';
-    document.body.style.opacity = 1;
-  }, 100);
-});
